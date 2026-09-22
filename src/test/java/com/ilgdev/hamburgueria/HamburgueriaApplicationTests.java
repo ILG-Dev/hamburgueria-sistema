6288596879aaf49e1f0b3282645fa3d4d10ae3ec
Ivan Lopes Guimarães <contato.ilg.dev@gmail.com>
2026-09-21 21:48:42 -0300

    Projeto inicial Spring Boot

---
+++ src/test/java/com/ilgdev/hamburgueria/HamburgueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilgdev.hamburgueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamburgueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
